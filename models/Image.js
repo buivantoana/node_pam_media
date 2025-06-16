4470b2f838fb76bb58740ebee3faff476132fbe3
buivantoana <toanbui219@gmail.com>
2025-06-16 22:29:22 +0700

    model image

--- models/Image.js
+++ models/Image.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dynamicImageSchema = new mongoose.Schema(
   {
     name: { type: String, required: true }, // "recruitment", "cooperate", "home", etc.
-    
+
     recruitment: {
       rightImage1: String,
       leftImage2: String,
@@ -13,13 +13,29 @@ const dynamicImageSchema = new mongoose.Schema(
       image1: String,
       image2: String,
       image3: String,
+      image4: String,
+      image5: String,
+      image6: String,
+      image7: String,
+      image8: String,
+      image9: String,
       topImage: String,
       title: String,
       description: String,
+      title1: String,
+      title2: String,
+      title3: String,
+      title4: String,
+      title5: String,
+      title6: String,
+      title7: String,
+      title8: String,
+      title9: String,
     },
 
     homeBanner: {
       rightImage1: String,
+      bottomImage: String,
       leftImage2: String,
       bannerLeftTop: String,
       bannerLeftBottom: String,
